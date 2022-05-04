ac9d059d988e55c534a9b4f36592e8dfcb06e784
abedareNasuni <101194763+abedareNasuni@users.noreply.github.com>
2022-05-04 16:20:29 +0530

    update prov-nac.sh

--- prov-nac.sh
+++ prov-nac.sh
@@ -1,16 +1,10 @@
 #!/bin/bash/
-# CONFIG_PATH=$1
 
 echo "INFO ::: Encrypting config.dat file : STARTED"
+
 nac_manager encrypt -c config.dat -p pass@123
+
 echo "INFO ::: Encrypting config.dat file : COMPLETED"
 echo "INFO ::: NAC Deployment : STARTED"
 
-STATUS=`nac_manager deploy -c config.dat -p pass@123`
-echo "$STATUS" 
-if [ "$STATUS" == "0" ]; then 
-    echo "INFO ::: NAC Deployment : COMPLETED"
-else
-    echo "ERROR ::: NAC Deployment : FAILED"       
-fi 
-
+nac_manager deploy -c config.dat -p pass@123
