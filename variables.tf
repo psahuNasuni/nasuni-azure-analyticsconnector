fd17f15f35579dfbdfe8b8460217966c9426b6c3
Ubuntu <azureuser@myvm.sksdz3hosyievhbbpcxcwph5nd.bx.internal.cloudapp.net>
2022-05-10 14:19:07 +0000

    Updated variables

--- variables.tf
+++ variables.tf
@@ -7,29 +7,21 @@
 variable "acs_resource_group" {
   description = "Resouce group name for Azure Cognitive Search"
   type        = string
-  default     = "testfunctionrg"
-}
-
-variable "acs_service_name" {
-  description = "Service name for Azure Cognitive Search"
-  type        = string
-  default     = "demo"
 }
 
 variable "azure_location" {
   description = "Region for Azure Cognitive Search"
   type        = string
-  default     = "eastus"
+}
+
+variable "acs_key_vault" {
+  description = "Azure Key Vault name for Azure Cognitive Search"
+  type        = string
+  default     = "nasuniacssecretstore"
 }
 
 variable "output_path" {
   type        = string
   description = "function_path of file where zip file is stored"
   default     = "./ACSFunction.zip"
-}
-  
-variable "acs_key_vault" {
-  description = "Azure Key Vault name for Azure Cognitive Search"
-  type        = string
-  default     = "nasuniacssecretstore"
 }
