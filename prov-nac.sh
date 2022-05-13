df53bfb7b0dbeaac7085753b5575c40fe5f4be6b
Ubuntu <azureuser@myvm.sksdz3hosyievhbbpcxcwph5nd.bx.internal.cloudapp.net>
2022-05-13 06:15:28 +0000

    Updated config.dat file

--- prov-nac.sh
+++ prov-nac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 echo "INFO ::: Encrypting config.dat file : STARTED"
-nac_manager encrypt -c /usr/local/bin/config.dat -p pass@123
+nac_manager encrypt -c config.dat -p pass@123
 echo "INFO ::: Encrypting config.dat file : COMPLETED"
 echo "INFO ::: NAC Deployment : STARTED"
-nac_manager deploy -c /usr/local/bin/config.dat -p pass@123
+nac_manager deploy -c config.dat -p pass@123
