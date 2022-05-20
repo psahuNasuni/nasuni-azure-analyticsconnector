ce4983af9aece46a26dab9cc4bca26bfe97ea935
Sachin Gaikwad <sachin.gaikwad@nasuni.com>
2022-05-20 13:50:14 +0530

    Updated prov-nac.sh password

--- prov-nac.sh
+++ prov-nac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash/
 echo "INFO ::: Encrypting config.dat file : STARTED"
-nac_manager encrypt -c config.dat -p pass@1234
+nac_manager encrypt -c config.dat -p pass@12345
 echo "INFO ::: Encrypting config.dat file : COMPLETED"
 echo "INFO ::: NAC Deployment : STARTED"
-nac_manager deploy -c config.dat -p pass@1234
+nac_manager deploy -c config.dat -p pass@12345
+
