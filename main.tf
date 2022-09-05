8d6805076151b7b5e25ed429c5f05720ba739a4a
aniket <abedare@nasuni.com>
2022-09-05 16:33:37 +0530

    updated quotes

--- main.tf
+++ main.tf
@@ -162,7 +162,7 @@ resource "azurerm_app_configuration_key" "unifs-toc-handle" {
 ########## START : Run NAC Discovery Function ###########################
 resource "null_resource" "run_discovery_function" {
   provisioner "local-exec" {
-    command = "curl -X GET -H \"Content-Type:application/json\" \"https://${azurerm_linux_function_app.discovery_function_app.default_hostname}/api/IndexFunction\""
+    command = "curl -X GET -H 'Content-Type:application/json' 'https://${azurerm_linux_function_app.discovery_function_app.default_hostname}/api/IndexFunction'"
   }
   depends_on = [
     null_resource.set_env_variable,
