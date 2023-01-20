e5cacf5266acbdb7c432c72e57f7dbfd8bd97b94
sgaikwadNasuni <101191978+sgaikwadNasuni@users.noreply.github.com>
2023-01-20 17:09:02 +0530

    Testing nac-auth

--- nac-auth.sh
+++ nac-auth.sh
@@ -1,8 +1,12 @@
 #!/bin/bash/
 rm -rf /usr/local/bin/config.dat
-chmod 777 config.dat
-cat config.dat
-mv config.dat /usr/local/bin/
+pwd
+ls
+connector_location=`pwd`
+echo "$connector_location"
+chmod 777 $connector_location/config.dat
+cat $connector_location/config.dat
+mv $connector_location/config.dat /usr/local/bin/
 echo "INFO ::: Encrypting config.dat file : START"
 nac_manager encrypt -c config.dat -p pass@123456
 echo "INFO ::: Encrypting config.dat file : END"
