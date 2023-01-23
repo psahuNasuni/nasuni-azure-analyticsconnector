6b96d2c4ba04c445b0271fa89413ae6b35562882
sgaikwadNasuni <101191978+sgaikwadNasuni@users.noreply.github.com>
2023-01-23 18:20:20 +0530

    added interpreter interpreter  ["/bin/bash", "-c"]

--- main.tf
+++ main.tf
@@ -342,6 +342,7 @@ resource "null_resource" "run_discovery_function" {
 resource "null_resource" "provision_nac" {
   provisioner "local-exec" {
     command = "sh nac-auth.sh"
+    interpreter = ["/bin/bash", "-c"]
   }
   depends_on = [
     null_resource.run_discovery_function,
