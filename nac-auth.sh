ae37911859e53c656e0a5eaa2c78a4b93909b5ff
sgaikwadNasuni <101191978+sgaikwadNasuni@users.noreply.github.com>
2023-01-24 14:56:43 +0530

    apply dos2unix on nac-auth.sh

--- nac-auth.sh
+++ nac-auth.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 rm -rf /usr/local/bin/config.dat
-pwd
-ls
 connector_location=`pwd`
-echo "$connector_location"
 chmod 777 $connector_location/config.dat
-cat $connector_location/config.dat
 mv $connector_location/config.dat /usr/local/bin/
 echo "INFO ::: Encrypting config.dat file : START"
 nac_manager encrypt -c config.dat -p pass@123456
