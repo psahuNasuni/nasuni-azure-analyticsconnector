ce901681d6543b3c922d2cad379974b93ce80ee6
unknown <ajain@nasuni.com>
2023-07-18 14:08:45 +0530

    reverting file name test changes

--- nac-auth.sh
+++ nac-auth.sh
@@ -11,12 +11,12 @@ do
         rm -rf /usr/local/bin/config.dat
         connector_location=`pwd`
         chmod 777 $connector_location/config.dat
-        mv $connector_location/config.dat /usr/local/bin/share_data_01.dat
+        mv $connector_location/config.dat /usr/local/bin/config.dat
         echo "INFO ::: Encrypting config.dat file : START"
-        nac_manager encrypt -c share_data_01.dat -p pass@123456
+        nac_manager encrypt -c config.dat -p pass@123456
         echo "INFO ::: Encrypting config.dat file : END"
         echo "INFO ::: NAC Deployment : STARTED ........."
-        nac_manager deploy -c share_data_01.dat -p pass@123456
+        nac_manager deploy -c config.dat -p pass@123456
         echo "INFO ::: NAC Deployment : COMPLETED !!!"
         break
     else
