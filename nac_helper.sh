b370df54e26bf77221e519c73f4820ecf8c9beca
Pradeepta Sahu <psahu@nasuni.com>
2023-09-13 17:48:34 +0530

    dev/null added

--- nac_helper.sh
+++ nac_helper.sh
@@ -10,7 +10,7 @@ echo " Argument received is : $resource_group"
 get_resource_list () {
     resource_list_json=""
     while [ -z "$resource_list_json" ]; do
-        resource_list_json=$(az resource list --resource-group "$resource_group")
+        resource_list_json=$(az resource list --resource-group "$resource_group" 2> /dev/null)
     done
 }
 
@@ -34,21 +34,6 @@ while [ -z "$cosmosdb_account_name" ]; do
 done
 
 sleep 600
-
-#while true; do
-  #db_state=$(az cosmosdb show --name "$cosmosdb_account_name" --resource-group "$resource_group" --query "provisioningState" -o tsv)
-  
-  #if [ "$db_state" == "Succeeded" ]; then
-    #echo "Cosmos DB provisioning is completed"
-    #break
-  #elif [ "$db_state" == "Creating" ] || [ "$db_state" == "Updating" ]; then
-    #echo "Cosmos DB provisioning state is $db_state. Waiting for 1 minute to re-check the state"
-    #sleep 60
-  #else
-    #echo "Cosmos DB provisioning state is $db_state. Exiting the nac_helper script"
-    #exit 1
-  #fi
-#done
 
 echo "Trying to retrieve count of objects in cosmos db"
 
