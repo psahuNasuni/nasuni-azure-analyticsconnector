9a70406276fc1a66373ed4cb69f9cfc97b58ea27
Arpit Jain <ajain@nasuni.com>
2024-02-19 16:48:33 +0530

    Refactored log messages in nac_helper.sh

--- nac_helper.sh
+++ nac_helper.sh
@@ -11,11 +11,11 @@ cosmosdb_count=0
 stop_nac_process() {
     pgrep -f 'nac_manager' > nac_manager_pids.tmp
     while read -r pid; do
-        echo "Killing process with PID: $pid"
+        echo "INFO ::: Killing process with PID: $pid"
         kill "$pid"
     done < nac_manager_pids.tmp
     rm nac_manager_pids.tmp
-    echo "aborting nac process"
+    echo "ERROR ::: aborting nac process"
     exit 1
 }
 
@@ -24,7 +24,7 @@ get_storage_account_object_count(){
 	destination_storage_conn_str=$(az storage account show-connection-string --name ${destination_storage_acc_name} | jq -r '.connectionString')
 	scontainer_object_count=0
 
-    echo "Checking count of objects in cosmosdb"
+    echo "INFO ::: Checking count of objects in cosmosdb"
 	while [ "$NEXTMARKER" != "null" ]; do
 		if [ -n "$NEXTMARKER" ]; then
 
@@ -39,31 +39,31 @@ get_storage_account_object_count(){
 		NEXTMARKER=$(echo $FILES | jq -r '.[-1].nextMarker')
 	done
 	scontainer_object_count=$((scontainer_object_count - 1))
-	echo "Count of objects in destination storage container: $scontainer_object_count"
+	echo "INFO ::: Count of objects in destination storage container: $scontainer_object_count"
 	
 }
 
 get_resource_list () {
     resource_list_json=""
-    max_attempts=2
+    max_attempts=3
     current_attempt=1
 
     while [ -z "$resource_list_json" ] && [ "$current_attempt" -le "$max_attempts" ]; do
         sleep 60
         resource_list_json=$(az resource list --resource-group "$resource_group" 2> /dev/null)
-        echo "Retrieving resource list (Attempt $current_attempt)"
+        echo "INFO ::: Retrieving resource list (Attempt $current_attempt)"
         
         if [ -z "$resource_list_json" ]; then      
-            ((current_attempt++))
+            current_attempt=$((current_attempt+1))
         fi
     done
 
     if [ -z "$resource_list_json" ]; then
-        echo "Failed to retrieve resource list after $max_attempts attempts. Exiting script."
+        echo "ERROR ::: Failed to retrieve resource list after $max_attempts attempts. Exiting script."
         stop_nac_process
         exit 1
     else
-        echo "Successfully retrieved resource list"
+        echo "INFO ::: Successfully retrieved resource list"
     fi
 }
 
@@ -71,16 +71,16 @@ check_storage_account_existence() {
     storage_account_name="$1"
 
     if az storage account show --name "$storage_account_name" --query "name" --output tsv 2>/dev/null; then
-        echo "Storage account '$storage_account_name' exists."
+        echo "INFO ::: Storage account '$storage_account_name' exists."
     else
-        echo "Storage account '$storage_account_name' not found. Exiting script."
+        echo "ERROR ::: Storage account '$storage_account_name' not found. Exiting script."
         stop_nac_process
         exit 1        
     fi
 }
 
 get_cosmosdb_document_count() {
-    echo "Trying to retrieve count of objects in cosmos db"
+    echo "INFO ::: Trying to retrieve count of objects in cosmos db"
     result=$(az cosmosdb sql container show --account-name "$cosmosdb_account_name" --resource-group "$resource_group" --database-name "$database_name" --name "$container_name" 2> /dev/null)
     cosmosdb_count=$(echo "$result" | jq -r '.resource.statistics[].documentCount' | awk '{s+=$1} END {print s}')
 }
@@ -93,13 +93,13 @@ current_minute=1
 while [ -z "$cosmosdb_account_name" ]; do
 
     cosmosdb_account_name=$(echo "$resource_list_json" | jq -r '.[] | select(.type == "Microsoft.DocumentDb/databaseAccounts") | .name')
-    echo "Cosmos DB Account Name: $cosmosdb_account_name"
+    echo "INFO ::: Cosmos DB Account Name: $cosmosdb_account_name"
 
     if [ -n "$cosmosdb_account_name" ]; then
-        echo "Cosmos DB has been created."
+        echo "INFO ::: Cosmos DB has been created."
         break
     else
-        echo "Check $current_minute Cosmos DB has not been created yet."
+        echo "INFO ::: Check $current_minute: Cosmos DB has not been created yet."
         sleep 60
         current_minute=$((current_minute + 1))
         get_resource_list
@@ -111,21 +111,21 @@ sleep 600
 while true; do
   state=$(az cosmosdb show --name "$cosmosdb_account_name" --resource-group "$resource_group" --query "provisioningState" | tr -d '"')
   
-echo "state is : $state"
+echo "INFO ::: CosmosDB's state is : $state"
 
   if [ -n "$state" ]; then
 
   case "$state" in
   "Succeeded")
-    echo "Cosmos DB provisioning state is Succeeded."
+    echo "INFO ::: Cosmos DB provisioning state is Succeeded."
     break
     ;;
   "Creating" | "Updating")
-    echo "Cosmos DB provisioning state is $state. Waiting for 1 minute to re-check"
+    echo "INFO ::: Cosmos DB provisioning state is $state. Waiting for 1 minute to re-check"
     sleep 60
     ;;
   *)
-    echo "Cosmos DB provisioning state is $state. Exiting..."
+    echo "WARNING ::: Cosmos DB provisioning state is $state. Exiting nac_helper script..."
     exit 1
     ;;
 esac
@@ -137,13 +137,13 @@ get_storage_account_object_count
 get_cosmosdb_document_count
 
 if [ -z "$scontainer_object_count" ]; then
-        echo "No data found in destination storage account."
+        echo "ERROR ::: No data found in destination storage account."
         stop_nac_process
     fi
 
 
 if [ "$cosmosdb_count" -lt 1 ]; then
-        echo "Document count in cosmosdb is less than 1. Exiting the script."
+        echo "ERROR ::: Document count in cosmosdb is less than 1. Exiting the script."
         stop_nac_process
 else
     counter=0
@@ -163,14 +163,14 @@ else
         if [ "$new_container_count" -eq "$previous_container_count" ]; then
             if [ "$new_db_count" -eq "$previous_db_count" ]; then
 
-                echo "Subsequent Count of objects in cosmosdb and destination container are same. Exiting the nac_manager script."
+                echo "ERROR ::: Subsequent Count of objects in cosmosdb and destination container are same. Exiting the nac_manager script."
                 stop_nac_process
                 exit 1
             fi
         fi
 
-        echo "Document Count in $database_name/$container_name: $cosmosdb_count"
-        echo "Document Count in $destination_storage_acc_name/destcontainer:$scontainer_object_count"
+        echo "INFO ::: Document Count in $database_name/$container_name: $cosmosdb_count"
+        echo "INFO ::: Document Count in $destination_storage_acc_name/destcontainer:$scontainer_object_count"
         
         counter=$((counter+1))
         previous_db_count="$new_db_count"
@@ -178,5 +178,5 @@ else
 
     done
 
-    echo "Exiting the nac_helper script."
+    echo "INFO ::: Exiting the nac_helper script."
 fi
